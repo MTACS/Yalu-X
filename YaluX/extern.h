686842cd3ee06eda535cdb349c7563a0b6894bcb
MTAC <mtactheming@gmail.com>
2017-07-31 08:52:04 -0400

    no message

---
+++ YaluX/extern.h
@@ -0,0 +1,12 @@
+//
+//  extern.h
+//  YaluX
+//
+//  Created by MTAC on 7/28/17.
+//  Copyright © 2017 kimjongcracks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+extern NSInteger ssh;
+extern NSInteger substrate;
