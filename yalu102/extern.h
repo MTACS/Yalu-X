37f795b0ff81574f0e32bcc674fa602e54302a46
MTACS <derekaforesman@gmail.com>
2017-11-20 13:44:15 -0500

    Added files via upload

---
+++ yalu102/extern.h
@@ -0,0 +1,13 @@
+//
+//  extern.h
+//  yalu102
+//
+//  Created by MTAC on 11/20/17.
+//  Copyright © 2017 MTAC Research. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+extern NSInteger reinstall; // I'll add this functionality later
+extern NSInteger ssh;
+extern NSInteger substrate; // Same with this one, I don't have time
